255dd1d3cd75978aab4841a21204a8571f50b715
ShanavasS97 <d2p10shana@gmail.com>
2022-01-01 12:52:50 +0530

    java-project

---
+++ src/org/test/A.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class A {
+	 
+	
+ public static void main(String[] args) {
+	
+}
+		
+	}
+
+
---
+++ src/org/test/B.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class B {
+	public int M1(String a) {
+		int i = a.length();
+		return i;
+	}
+
+}
